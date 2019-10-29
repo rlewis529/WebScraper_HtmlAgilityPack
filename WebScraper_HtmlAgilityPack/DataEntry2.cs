9d873c60b595630c8dc0b51c218366148bea5a61
Robert Lewis <rlewis@relias.com>
2019-10-28 22:41:14 -0400

    adding second web scraper step for game results

---
+++ WebScraper_HtmlAgilityPack/DataEntry2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebScraper_HtmlAgilityPack
+{
+    public class DataEntry2
+    {
+        public string TeamName { get; set; }
+        public string WeekName { get; set; }
+        public string Opponent { get; set; }
+        public string OpponentRank { get; set; }
+        public string GameLocation { get; set; }
+        public string GameResult { get; set; }
+    }
+}
--- WebScraper_HtmlAgilityPack/Program.cs
+++ WebScraper_HtmlAgilityPack/Program.cs
@@ -9,35 +9,28 @@ using System.Text;
 //https://www.c-sharpcorner.com/article/web-scraping-in-c-sharp/
 //Example link:  https://www.teamrankings.com/college-football/team/louisiana-state-tigers/stats
 
-//Next steps:  
-// Import text file into SQL table(s)?
-
 namespace WebScraper_HtmlAgilityPack
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Accessing source webpages and scraping data...");
+            Console.WriteLine("Accessing source webpages and scraping data...");            
             Console.WriteLine();
-            Console.WriteLine("Data collected from...");
+            Console.WriteLine("In-game statistics collected from...");
             var teamList = new List<string>();                       
             //teamList.Add("alabama-crimson-tide");
             //teamList.Add("louisiana-state-tigers");
             var teamListImport = new List<string>();
             teamListImport = File.ReadLines("TeamNames.csv").Skip(1).ToList();        
 
-            
             HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
-            var rawList = new List<DataEntry>();
-            var rawList2 = new List<DataEntry>();
-            var consolidatedList = new List<DataEntry>();
+            var rawList = new List<DataEntry>();                        
             var sb = new StringBuilder();
-            var sb2 = new StringBuilder();
 
-            for (int teamNumber = 0; teamNumber < teamListImport.Count; teamNumber++)            
+            for (int teamNumber = 0; teamNumber < teamListImport.Count; teamNumber++)
             {
-                rawList.Clear();               
+                rawList.Clear();
                 string scrapeLink = "https://www.teamrankings.com/college-football/team/" + teamListImport[teamNumber] + "/stats";
                 HtmlAgilityPack.HtmlDocument doc = web.Load(scrapeLink);
                 var headerText = doc.DocumentNode.SelectSingleNode("//h1[@id='h1-title']").InnerHtml;
@@ -57,17 +50,68 @@ namespace WebScraper_HtmlAgilityPack
                         rawList[i / 2].Value = dataValues[i].InnerText;
                     }
                 }
-                consolidatedList.AddRange(rawList);  
+                
                 foreach (var data in rawList)
                 {
                     sb.AppendLine(data.TeamName + "," + data.StatisticNumber + "," + data.Statistic + "," + data.Value);
                 }
                 Console.WriteLine(teamListImport[teamNumber]);
             }
-            
+
             File.WriteAllText("testOutput.csv", sb.ToString());
             Console.WriteLine();
-            Console.WriteLine("*** Data collection complete! ***");
+            Console.WriteLine("*** In-game statistic data collection complete! ***");
+            Console.WriteLine();
+            Console.WriteLine("Scraping data for game results...");
+            Console.WriteLine();
+            Console.WriteLine("Game results collected from...");
+
+            var rawList2 = new List<DataEntry2>();            
+            var sb2 = new StringBuilder();
+
+            for (int teamNumber2 = 0; teamNumber2 < teamListImport.Count; teamNumber2++)            
+            {
+                rawList2.Clear();
+                string scrapeLink2 = "https://www.teamrankings.com/college-football/team/" + teamListImport[teamNumber2] + "/game-log";
+                HtmlAgilityPack.HtmlDocument doc2 = web.Load(scrapeLink2);
+                var headerText2 = doc2.DocumentNode.SelectSingleNode("//h1[@id='h1-title']").InnerHtml;
+                headerText2 = headerText2.Replace(" Performance", "");
+                var dataValues2 = doc2.DocumentNode.SelectNodes("//table//tr//td");
+                                
+                for (int j = 0; j < dataValues2.Count; j++)
+                {
+                    if (j % 5 == 0)
+                    {
+                        rawList2.Add(new DataEntry2 { TeamName = headerText2 });
+                        rawList2[j/5].WeekName = dataValues2[j].InnerText;
+                    }
+                    if (j % 5 == 1)
+                    {                        
+                        rawList2[j / 5].Opponent = dataValues2[j].InnerText;
+                    }
+                    if (j % 5 == 2)
+                    {
+                        rawList2[j / 5].OpponentRank = dataValues2[j].InnerText;
+                    }
+                    if (j % 5 == 3)
+                    {
+                        rawList2[j / 5].GameLocation = dataValues2[j].InnerText;
+                    }
+                    if (j % 5 == 4)
+                    {
+                        rawList2[j / 5].GameResult = dataValues2[j].InnerText;
+                    }
+                }
+                
+                foreach (var data2 in rawList2)
+                {
+                    sb2.AppendLine(data2.TeamName + "," + data2.WeekName + "," + data2.Opponent + "," + data2.OpponentRank + "," + data2.GameLocation + "," + data2.GameResult);
+                }
+                Console.WriteLine(teamListImport[teamNumber2]);               
+            }            
+            File.WriteAllText("testOutput2.csv", sb2.ToString());
+            Console.WriteLine();
+            Console.WriteLine("*** Game result data collection complete! ***");
         }
     }
 }
